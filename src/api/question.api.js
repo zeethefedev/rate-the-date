c1669367732766ced40cb95dcf8fb4d084ac5252
zeethefedev <zeethefrontenddev@gmail.com>
2024-04-23 22:07:28 +0700

    add env

--- src/api/question.api.js
+++ src/api/question.api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { BASE_URL } from "./constant.api";
+import { baseUrl } from "./constant.api";
 
 export const fetchFormsApi = () => {
-  return axios.get(`${BASE_URL}/all-forms`);
+  return axios.get(`${baseUrl}/all-forms`);
 };
 
 //POST
 export const postFormApi = (form) => {
-  return axios.post(`${BASE_URL}/add-form`, form);
+  return axios.post(`${baseUrl}/add-form`, form);
 };
--- src/api/response.api.js
+++ src/api/response.api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
-import { BASE_URL } from "./constant.api";
+import { baseUrl } from "./constant.api";
 
 //GET BY ID
 export const fetchFormApi = (Id) => {
-  return axios.get(`${BASE_URL}/form/${Id}`);
+  const api = `${baseUrl}/form/${Id}`;
+  debugger;
+  return axios.get(`${baseUrl}/form/${Id}`);
 };
 
 //PATCH
 export const updateFormApi = (form) => {
-  return axios.patch(`${BASE_URL}/edit-form/${form.id}`, {
+  return axios.patch(`${baseUrl}/edit-form/${form.id}`, {
     questions: form.questions,
   });
 };
